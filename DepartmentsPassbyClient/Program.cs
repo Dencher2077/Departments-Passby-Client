4230f25181e4b8c9c08183bb61c2ce9ec2a21e41
Dencher12 <dencher12@protonmail.com>
2021-04-11 15:53:35 +0300

    Init

---
+++ DepartmentsPassbyClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DepartmentsPassby;
+
+namespace DepartmentsPassbyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PassbyResult result = new Passby("config.json").Start("Department 3");
+
+            Console.WriteLine("Seals snapshots:");
+            foreach (var sealsSnapshot in result.SealsSnapshots)
+            {
+                foreach (var seal in sealsSnapshot)
+                {
+                    Console.Write(seal + " ");
+                }
+
+                Console.WriteLine();
+            }
+            
+
+            Console.WriteLine("\nMessages:");
+            foreach (var msg in result.Messages)
+            {
+                Console.WriteLine(msg);
+            }
+        }
+    }
+}
